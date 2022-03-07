c9028960bf5833b730525e95a8ffc3c722afedcf
Onekki <285079772@qq.com>
2022-03-07 16:04:17 +0800

    feat(star-ark): add star-ark

---
+++ star-ark/main.js
@@ -0,0 +1,59 @@
+const axios = require('axios')
+const config = require('./config')
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(() => resolve()), ms)
+}
+
+const request = async (options) => {
+    try {
+        const response = await axios.request(options)
+        console.log(response.data)
+        const code = response.data.code
+        if ([1].includes(code)) {
+            return response.data
+        }
+    } catch (e) { 
+        console.log(e.message)
+    }
+    await sleep(100)
+    return await request(options)
+}
+
+const start = async () => {
+    console.log('start')
+
+    let data = await request({
+        url: 'https://h5.stararknft.art/api/Box/detailed',
+        method: 'POST',
+        headers: config.headers,
+        data: 'id=' + config.id + '&uid=' + config.uid
+    })
+
+    const token_id = data.data.info.token_id
+
+    data = await request({
+        url: 'https://h5.stararknft.art/api/Pay/before_buy_box',
+        method: 'POST',
+        headers: config.headers,
+        data: 'id=' + config.id +'&uid=' + config.uid + '&token_id=' + token_id
+    })
+
+    data = await request({
+        url: 'https://h5.stararknft.art/api/Pay/direct_buy_box',
+        method: 'POST',
+        headers: config.headers,
+        data: 'id='+ config.id + 
+                '&uid=' + config.uid + 
+                '&money=' + config.price + 
+                '&token_id='+ token_id + 
+                '&password=' + config.password
+    })
+    
+    console.log(data)
+    
+    console.log('end')
+}
+
+start()
+
