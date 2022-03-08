eb9f92117aa42c1bf31e07101d949a041c7700c2
Onekki <285079772@qq.com>
2022-03-08 13:35:56 +0000

    feat(meta-h5): add meta-h5

---
+++ meta-h5/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    pid: '156',
+    headers: {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "access-control-allow-method": "POST,GET,DELETE,OPTIONS",
+        "access-control-allow-origin": "*",
+        "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjQzNTI4LCJzY29wZSI6OCwiZXhwIjoxNzMzMTAyMDUyLCJpYXQiOjE2NDY3MDIwNTJ9.Lb80SIWjjPzX0OwG9y-k8ig-l0NUub55GIyJFGwcaok",
+        "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Microsoft Edge\";v=\"99\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "x-requested-with": "XMLHttpRequest",
+        "Referer": "https://meta-h5.genimous.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    }
+}
---
+++ meta-h5/main.js
@@ -0,0 +1,81 @@
+const axios = require('axios')
+const config = require('./config')
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(() => resolve()), ms)
+}
+
+const request = async (options) => {
+    try {
+        const response = await axios.request(options)
+        console.log(response.data)
+        const { status } = response.data
+        if ([200].includes(status)) {
+            return response.data
+        }
+    } catch (e) { 
+        console.log(e.message)
+    }
+    await sleep(1000)
+    return await request(options)
+}
+
+const start = async () => {
+    console.log('start')
+
+    let data = await request({
+        method: 'GET',
+        url: 'https://meta-art.genimous.com/api/goods/detail/' + config.pid + '?apiSource=client',
+        headers: config.headers
+    })
+
+    const unique = data.data.products[0].unique
+    
+    data = await request({
+        method: 'POST',
+        url: 'https://meta-art.genimous.com/api/cart/add',
+        data: {
+            cartNum: 1,
+            productId: config.pid,
+            uniqueId: unique
+        },
+        headers: config.headers
+    })
+
+    const cartId = data.cartId
+
+    data = await request({
+        method: 'POST',
+        url: 'https://meta-art.genimous.com/api/order/confirm',
+        data: {
+            cartId: cartId
+        },
+        headers: config.headers
+    })
+
+    const orderKey = data.orderKey
+
+    data = await request({
+        method: 'POST',
+        url: 'https://meta-art.genimous.com/api/order/computed/' + orderKey,
+        data: {
+            couponId: '0'
+        },
+        headers: config.headers
+    })
+
+    await request({
+        method: 'POST',
+        url: 'https://meta-art.genimous.com/api/order/create/' + orderKey,
+        data: {
+            couponId: '0',
+            from: 'weixinh5',
+            isPay: '0',
+            payType: 'weixin'
+        }
+    })
+
+    console.log('end')
+}
+
+start()
