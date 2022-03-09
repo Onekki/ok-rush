246e0771eba845d905a0dc0214d0c7a50e058e35
Onekki <285079772@qq.com>
2022-03-09 11:56:54 +0800

    fix(star-ark): sleep not work

--- star-ark/main.js
+++ star-ark/main.js
@@ -2,17 +2,18 @@ const axios = require('axios')
 const config = require('./config')
 
 const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(() => resolve()), ms)
+    return new Promise(resolve => setTimeout(() => resolve(), ms))
 }
 
 const request = async (options) => {
     try {
         const response = await axios.request(options)
-        console.log(response.data)
         const code = response.data.code
         if ([1].includes(code)) {
+            console.log(response.data)
             return response.data
         }
+        console.log(JSON.stringify(response.data))
     } catch (e) { 
         console.log(e.message)
     }
